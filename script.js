3028108033921fa551c155184581d05e5b49aec8
Okolo Michael <Okolo.m@ieslglobal.com>
2023-01-09 15:42:18 +0100

    eventlisteners, functions created

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const textArea = document.querySelector('textarea');
 const originText = document.querySelector('.text');
 const resetButton = document.querySelector('.btn');
 const theTimer = document.querySelector('.timer');
+
+// Add leading zero to numbers 9
+//Create a Clock
+//Reset everything function
+function reset(){
+    
+}
+//Match the text with provided text on the page
+function spellCheck(){
+    
+}
+//Start the timer function
+function start(){
+
+}
+//Eventlisteners for keyboad input and reset button event
+textArea.addEventListener('keypress', start , false);
+textArea.addEventListener('keyup', spellCheck, false);
+resetButton.addEventListener('click', reset, false)
