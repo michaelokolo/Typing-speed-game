db00d6e4e20197232a66b6b3b25ef18010e834ac
Okolo Michael <Okolo.m@ieslglobal.com>
2023-01-09 15:29:22 +0100

    started the scripting

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const textWrapper = document.querySelector('.text-wrapper');
-console.log(textWrapper);
+const textArea = document.querySelector('textarea');
+const originText = document.querySelector('.text');
+const resetButton = document.querySelector('.btn');
+const theTimer = document.querySelector('.timer');
